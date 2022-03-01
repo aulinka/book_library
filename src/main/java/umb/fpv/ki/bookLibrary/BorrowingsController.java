962b0ab80f50378dc91207501e5681748fd3d7b4
aulinka <laura.mikusova27@gmail.com>
2022-03-01 22:27:14 +0100

    borrowings service

--- src/main/java/umb/fpv/ki/bookLibrary/BorrowingsController.java
+++ src/main/java/umb/fpv/ki/bookLibrary/BorrowingsController.java
@@ -5,36 +5,23 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-class CreateBorrowingBody{
-    public long customerId;
-    public long bookId;
-}
-
 @RestController
 public class BorrowingsController {
-    public static final List<Borrowings> borrowings = new ArrayList();
 
     @PostMapping("/borrowings")
-    public Borrowings createBorrowing(@RequestBody CreateBorrowingBody body){
-        Borrowings b = new Borrowings();
-        Book pom =  BookController.books.stream().filter(book -> book.id == body.bookId).findFirst().get();
-        b.id = borrowings.size() + 1;
-        b.book = pom;
-        pom.bookCount--;
-        b.borrower = CustomerController.customers.stream().filter(customer -> customer.id == body.customerId).findFirst().get();
-        borrowings.add(b);
-        return b;
+    public void createBorrowing(@RequestBody Borrowings b) {
+        createBorrowing(b);
     }
     @GetMapping("/borrowings")
     public List<Borrowings> listBorrowings(){
-        return borrowings;
+        return listBorrowings();
     }
     @GetMapping("/borrowings/{borrowingId}")
     public Borrowings getBorrowingById(@PathVariable long borrowingId){
-        return borrowings.stream().filter(borrowing -> borrowing.id == borrowingId).findFirst().get();
+        return getBorrowingById(borrowingId);
     }
     @DeleteMapping("/borrowings/{borrowingId}")
     public void deleteBorrowingById(@PathVariable long borrowingId){
-        borrowings.removeIf(borrowing -> borrowing.id == borrowingId);
+       deleteBorrowingById(borrowingId);
     }
 }
---
+++ src/main/java/umb/fpv/ki/bookLibrary/BorrowingsService.java
@@ -0,0 +1,31 @@
+package umb.fpv.ki.bookLibrary;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BorrowingsService {
+    public  final List<Borrowings> borrowings = new ArrayList();
+    public BookService bookService;
+    public CustomerService customerService;
+    public void createBorrowing(Borrowings b){
+        borrowings.add(b);
+    }
+    public List<Borrowings> listBorrowings(){
+        return borrowings;
+    }
+    public Borrowings getBorrowingById( long borrowingId){
+        Borrowings b = new Borrowings();
+        Book book = bookService.getBookById(b.book.id);
+        Customer c = customerService.getCustomerById(b.borrower.id);
+        return b;
+    }
+    public void deleteBorrowingById(long borrowingId){
+        borrowings.removeIf(borrowing -> borrowing.id == borrowingId);
+    }
+
+
+}
