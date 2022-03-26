a1a76899f1e30fcec7e810454c60a9ae33280973
MichalToth5 <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>
2022-03-26 16:19:58 +0100

    dokončenie

--- src/main/java/umb/fpv/ki/bookLibrary/CustomerDTO.java
+++ src/main/java/umb/fpv/ki/bookLibrary/CustomerDTO.java
@@ -7,35 +7,4 @@ public class CustomerDTO {
     public String lastName;
     public  String email;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
